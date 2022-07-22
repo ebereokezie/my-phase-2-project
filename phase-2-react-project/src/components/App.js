78d712ecf4476e7723374a4fe59b9a9068ae4be0
Ebere Okezie <ebereokezie@gmail.com>
2022-07-21 21:14:10 -0500

    add image to homepage

--- phase-2-react-project/src/components/App.js
+++ phase-2-react-project/src/components/App.js
@@ -5,18 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <br></br>
+       <img src = "https://cdn.vox-cdn.com/thumbor/ytC-ZCsT-G-M1Fscy7oZUeZE9X0=/1400x788/filters:format(png)/cdn.vox-cdn.com/uploads/chorus_asset/file/19725578/TheRinger_Top25NBAPlayers_2.png" />
       </header>
     </div>
   );
