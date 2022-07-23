cdf1beec372584ef865bd83ef57dfa31cd2a4cac
Ebere Okezie <ebereokezie@gmail.com>
2022-07-23 08:28:53 -0500

    update home component

---
+++ phase-2-react-project/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import '../App.css'; 
+
+function Home(){
+    return(
+    <div className="Home">
+      <header className="Home-header">
+       <br></br>
+       <img src = "https://cdn.vox-cdn.com/thumbor/ytC-ZCsT-G-M1Fscy7oZUeZE9X0=/1400x788/filters:format(png)/cdn.vox-cdn.com/uploads/chorus_asset/file/19725578/TheRinger_Top25NBAPlayers_2.png" />
+      </header>
+    </div>
+    )
+}
